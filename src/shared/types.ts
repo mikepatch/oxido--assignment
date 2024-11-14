0c8ccdb2b6ff6f23ce1d273d75274114930b53e5
Michal Lata <michallata.dev@gmail.com>
2024-11-15 00:36:30 +0100

    feat: Add types

---
+++ src/shared/types.ts
@@ -0,0 +1,42 @@
+import OpenAI from "openai";
+
+export type Result<T> = {
+  data?: T;
+  error?: string;
+};
+
+export type ConvertArticleToHtmlConfig = {
+  inputRawTextPath: string;
+  htmlOutputPath: string;
+  completionModel: OpenAI.ChatModel;
+};
+
+export type ImageExtension = "webp" | "jpg" | "png";
+
+export type ImageGenerationServiceConfig = {
+  imagePrefix: string;
+  imageOutputPath: string;
+  imageExtension?: ImageExtension;
+};
+
+export type ConvertArticleWithGeneratedImagesConfig = {
+  htmlOutputPath: string;
+  imageModel: OpenAI.ImageModel;
+  imageSize: OpenAI.ImageEditParams["size"] | "1792x1024";
+  imageAltWithPromptRegex: RegExp;
+} & ImageGenerationServiceConfig;
+
+export type ImagePrompt = {
+  alt: string;
+  index: number;
+};
+
+export type AppConfig = {
+  fullArticleWithGeneratedImages: boolean;
+  articleToHtml: ConvertArticleToHtmlConfig;
+  articleWithGeneratedImages: ConvertArticleWithGeneratedImagesConfig;
+  openai: {
+    models: { completion: OpenAI.ChatModel; image: OpenAI.ImageModel };
+    imageSize: OpenAI.ImageEditParams["size"] | "1792x1024";
+  };
+};
