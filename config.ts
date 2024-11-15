d807954b42a0d3dcc695735a71b8e00fdcf908e1
Michal Lata <michallata.dev@gmail.com>
2024-11-15 07:04:40 +0100

    feat: Add APP_CONFIG

---
+++ config.ts
@@ -0,0 +1,20 @@
+import { AppConfig } from "./src/shared/types";
+
+// *Ustawienie "fullArticleWithGeneratedImages = true" zwiększy koszt ze względu na wykorzystanie modelu do generowania obrazów (0.18$ - 0.23$ przy 4-5 obrazkach)
+export const APP_CONFIG: AppConfig = {
+  fullArticleWithGeneratedImages: true, // *UWAGA! true = wyższy koszt
+  articleToHtml: {
+    inputRawTextPath: "src/example_raw_article.txt",
+    htmlOutputPath: "src/solution/artykul.html",
+    completionModel: "gpt-4o",
+  },
+  articleWithGeneratedImages: {
+    imageOutputPath: "src/bonus-generating-images/images",
+    htmlOutputPath: "src/bonus-generating-images/artykul.html",
+    imageModel: "dall-e-3",
+    imageSize: "1792x1024",
+    imagePrefix: "image",
+    imageExtension: "webp",
+    imageAltWithPromptRegex: /<img[^>]*alt="([^"]*)"[^>]*>/g,
+  },
+} as const;
